c6cab3792d15903462edfb40189d2ef085a091cb
Meisha Booker <meishabooker@meishas-mbp.attlocal.net>
2017-06-21 10:36:31 -0700

    add more insert info and update tables

--- Part-2/schema.sql
+++ Part-2/schema.sql
@@ -1,5 +1,18 @@
-CREATE TABLE grocery
+CREATE TABLE grocery.cvs
   (id INTEGER PRIMARY KEY,
   name TEXT,
   price INTEGER,
   section TEXT);
+
+CREATE TABLE shoppers.cvs
+  (id INTEGER PRIMARY KEY,
+  shopper_id INTEGER,
+  name TEXT,
+  age INTEGER,
+  habits TEXT);
+
+CREATE TABLE shoppers_orders.cvs
+  (id INTEGER PRIMARY KEY,
+  order_id INTEGER,
+  description TEXT,
+  total INTEGER);
